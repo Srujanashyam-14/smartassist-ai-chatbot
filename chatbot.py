612ee4e7709bf5c03c74b4dd021a34dee6feedfc
Srujanashyam-14 <srujanashyamd@gmail.com>
2026-04-28 19:31:52 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+
+# Add your API key here
+genai.configure(api_key="AIzaSyCdP04hl1qj2bbi3PiGuTkH32CVTzBUIdc")
+
+model = genai.GenerativeModel("gemini-pro")
+
+print("SmartAssist AI Chatbot (type 'exit' to stop)")
+
+while True:
+    user_input = input("You: ")
+    
+    if user_input.lower() == "exit":
+        print("Goodbye!")
+        break
+    
+    response = model.generate_content(user_input)
+    print("AI:", response.text)
